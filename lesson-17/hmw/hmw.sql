47860336fbac467c7ec3c41674151434eadf650d
Mukhiddin9777 <m_madrimov@mail.ru>
2025-05-23 16:51:20 +0500

    Update hmw.sql

--- lesson-17/hmw/hmw.sql
+++ lesson-17/hmw/hmw.sql
@@ -1,5 +1,5 @@
-1. You must provide a report of all distributors and their sales by region. If a distributor did not have any sales for a region, rovide a zero-dollar value for that day. Assume there is at least one sale for each region
-SQL Setup:
+----1. You must provide a report of all distributors and their sales by region. If a distributor did not have any sales for a region, rovide a zero-dollar value for that day. Assume there is at least one sale for each region
+----SQL Setup:
 
 DROP TABLE IF EXISTS #RegionSales;
 GO
@@ -14,7 +14,7 @@ INSERT INTO #RegionSales (Region, Distributor, Sales) VALUES
 ('North','ACE',10), ('South','ACE',67), ('East','ACE',54),
 ('North','ACME',65), ('South','ACME',9), ('East','ACME',1), ('West','ACME',7),
 ('North','Direct Parts',8), ('South','Direct Parts',7), ('West','Direct Parts',12);
-Select * from #RegionSales
+
 
 WITH AllRegions AS (
   SELECT DISTINCT Region FROM #RegionSales
@@ -37,38 +37,38 @@ SELECT * FROM FilledSales
 ORDER BY Distributor, Region;
 
 
-Input:
+----Input:
 
-|Region       |Distributor    | Sales |
-|-------------|---------------|--------
-|North        |ACE            |   10  |
-|South        |ACE            |   67  |
-|East         |ACE            |   54  |
-|North        |Direct Parts   |   8   |
-|South        |Direct Parts   |   7   |
-|West         |Direct Parts   |   12  |
-|North        |ACME           |   65  |
-|South        |ACME           |   9   |
-|East         |ACME           |   1   |
-|West         |ACME           |   7   |
-Expected Output:
+----|Region       |Distributor    | Sales |
+----|-------------|---------------|--------
+----|North        |ACE            |   10  |
+----|South        |ACE            |   67  |
+----|East         |ACE            |   54  |
+----|North        |Direct Parts   |   8   |
+----|South        |Direct Parts   |   7   |
+----|West         |Direct Parts   |   12  |
+----|North        |ACME           |   65  |
+----|South        |ACME           |   9   |
+----|East         |ACME           |   1   |
+----|West         |ACME           |   7   |
+----Expected Output:
 
-|Region |Distributor   | Sales |
-|-------|--------------|-------|
-|North  |ACE           | 10    |
-|South  |ACE           | 67    |
-|East   |ACE           | 54    |
-|West   |ACE           | 0     |
-|North  |Direct Parts  | 8     |
-|South  |Direct Parts  | 7     |
-|East   |Direct Parts  | 0     |
-|West   |Direct Parts  | 12    |
-|North  |ACME          | 65    |
-|South  |ACME          | 9     |
-|East   |ACME          | 1     |
-|West   |ACME          | 7     |
-2. Find managers with at least five direct reports
-SQL Setup:
+----|Region |Distributor   | Sales |
+----|-------|--------------|-------|
+----|North  |ACE           | 10    |
+----|South  |ACE           | 67    |
+----|East   |ACE           | 54    |
+----|West   |ACE           | 0     |
+----|North  |Direct Parts  | 8     |
+----|South  |Direct Parts  | 7     |
+----|East   |Direct Parts  | 0     |
+----|West   |Direct Parts  | 12    |
+----|North  |ACME          | 65    |
+----|South  |ACME          | 9     |
+----|East   |ACME          | 1     |
+----|West   |ACME          | 7     |
+----2. Find managers with at least five direct reports
+----SQL Setup:
 
 CREATE TABLE Employee (id INT, name VARCHAR(255), department VARCHAR(255), managerId INT);
 TRUNCATE TABLE Employee;
@@ -89,26 +89,26 @@ WHERE id IN (
 
 
 
-Input:
+----Input:
 
-| id  | name  | department | managerId |
-+-----+-------+------------+-----------+
-| 101 | John  | A          | null      |
-| 102 | Dan   | A          | 101       |
-| 103 | James | A          | 101       |
-| 104 | Amy   | A          | 101       |
-| 105 | Anne  | A          | 101       |
-| 106 | Ron   | B          | 101       |
-Expected Output:
+----| id  | name  | department | managerId |
+----+-----+-------+------------+-----------+
+----| 101 | John  | A          | null      |
+----| 102 | Dan   | A          | 101       |
+----| 103 | James | A          | 101       |
+----| 104 | Amy   | A          | 101       |
+----| 105 | Anne  | A          | 101       |
+----| 106 | Ron   | B          | 101       |
+----Expected Output:
 
-+------+
-| name |
-+------+
-| John |
-+------+
-You cal also solve this puzzle in Leetcode: https://leetcode.com/problems/managers-with-at-least-5-direct-reports/description/?envType=study-plan-v2&envId=top-sql-50
-3. Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
-SQL Setup:
+----+------+
+----| name |
+----+------+
+----| John |
+----+------+
+----You cal also solve this puzzle in Leetcode: https://leetcode.com/problems/managers-with-at-least-5-direct-reports/description/?envType=study-plan-v2&envId=top-sql-50
+----3. Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+----SQL Setup:
 
 CREATE TABLE Products (product_id INT, product_name VARCHAR(40), product_category VARCHAR(40));
 CREATE TABLE Orders (product_id INT, order_date DATE, unit INT);
@@ -124,8 +124,7 @@ INSERT INTO Orders VALUES
 (3,'2020-02-17',2),(3,'2020-02-24',3),
 (4,'2020-03-01',20),(4,'2020-03-04',30),(4,'2020-03-04',60),
 (5,'2020-02-25',50),(5,'2020-02-27',50),(5,'2020-03-01',50);
-Select * from Products
-Select * from Orders
+
 
 Select Product_name, sum(unit) TotUnit from Products as P
 join Orders as O
@@ -134,23 +133,23 @@ WHERE O.order_date BETWEEN '2020-02-01' AND '2020-02-29'
 group by Product_name
 having sum(unit) >=100
 
-Input:
+----Input:
 
-| product_id  | product_name          | product_category |
-+-------------+-----------------------+------------------+
-| 1           | Leetcode Solutions    | Book             |
-| 2           | Jewels of Stringology | Book             |
-| 3           | HP                    | Laptop           |
-| 4           | Lenovo                | Laptop           |
-| 5           | Leetcode Kit          | T-shirt          |
-Expected Output:
+----| product_id  | product_name          | product_category |
+----+-------------+-----------------------+------------------+
+----| 1           | Leetcode Solutions    | Book             |
+----| 2           | Jewels of Stringology | Book             |
+----| 3           | HP                    | Laptop           |
+----| 4           | Lenovo                | Laptop           |
+----| 5           | Leetcode Kit          | T-shirt          |
+----Expected Output:
 
-| product_name       | unit  |
-+--------------------+-------+
-| Leetcode Solutions | 130   |
-| Leetcode Kit       | 100   |
-4. Write an SQL statement that returns the vendor from which each customer has placed the most orders
-SQL Setup:
+----| product_name       | unit  |
+----+--------------------+-------+
+----| Leetcode Solutions | 130   |
+----| Leetcode Kit       | 100   |
+----4. Write an SQL statement that returns the vendor from which each customer has placed the most orders
+----SQL Setup:
 
 DROP TABLE IF EXISTS Orders;
 CREATE TABLE Orders (
@@ -162,7 +161,7 @@ CREATE TABLE Orders (
 INSERT INTO Orders VALUES
 (1,1001,12,'Direct Parts'), (2,1001,54,'Direct Parts'), (3,1001,32,'ACME'),
 (4,2002,7,'ACME'), (5,2002,16,'ACME'), (6,2002,5,'Direct Parts');
-Select * from Orders
+
 
 
 SELECT CustomerID, Vendor
@@ -176,28 +175,28 @@ FROM (
 WHERE rnk = 1;
 
 
-Input:
+----Input:
 
-|Order ID   | Customer ID | Order Count|     Vendor     |
----------------------------------------------------------
-|Ord195342  |     1001    |      12    |  Direct Parts  |
-|Ord245532  |     1001    |      54    |  Direct Parts  |
-|Ord344394  |     1001    |      32    |     ACME       |
-|Ord442423  |     2002    |      7     |     ACME       |
-|Ord524232  |     2002    |      16    |     ACME       |
-|Ord645363  |     2002    |      5     |  Direct Parts  |
-Expected Output:
+----|Order ID   | Customer ID | Order Count|     Vendor     |
+-------------------------------------------------------------
+----|Ord195342  |     1001    |      12    |  Direct Parts  |
+----|Ord245532  |     1001    |      54    |  Direct Parts  |
+----|Ord344394  |     1001    |      32    |     ACME       |
+----|Ord442423  |     2002    |      7     |     ACME       |
+----|Ord524232  |     2002    |      16    |     ACME       |
+----|Ord645363  |     2002    |      5     |  Direct Parts  |
+----Expected Output:
 
-| CustomerID | Vendor       |
-|------------|--------------|
-| 1001       | Direct Parts |
-| 2002       | ACME         |
-5. You will be given a number as a variable called @Check_Prime check if this number is prime then return 'This number is prime' else eturn 'This number is not prime'
-Example Input:
+----| CustomerID | Vendor       |
+----|------------|--------------|
+----| 1001       | Direct Parts |
+----| 2002       | ACME         |
+----5. You will be given a number as a variable called @Check_Prime check if this number is prime then return 'This number is prime' else eturn 'This number is not prime'
+----Example Input:
 
-DECLARE @Check_Prime INT = 91;
--- Your WHILE-based SQL logic here
-Expected Output:
+----DECLARE @Check_Prime INT = 91;
+---- Your WHILE-based SQL logic here
+----Expected Output:
 
 DECLARE @Check_Prime INT = 91;
 DECLARE @i INT = 2;
@@ -223,9 +222,9 @@ SELECT
   END AS Result;
 
 
-This number is not prime(or "This number is prime")
-6. Write an SQL query to return the number of locations,in which location most signals sent, and total number of signal for each device from the given table.
-SQL Setup:
+----This number is not prime(or "This number is prime")
+----6. Write an SQL query to return the number of locations,in which location most signals sent, and total number of signal for each device from the given table.
+----SQL Setup:
 
 CREATE TABLE Device(
   Device_id INT,
@@ -267,14 +266,14 @@ JOIN MaxLocation m ON a.Device_id = m.Device_id;
 
 
 
-Expected Output:
+----Expected Output:
 
-| Device_id | no_of_location | max_signal_location | no_of_signals |
-|-----------|----------------|---------------------|---------------|
-| 12        | 2              | Bangalore           | 6             |
-| 13        | 2              | Secunderabad        | 5             |
-7. Write a SQL to find all Employees who earn more than the average salary in their corresponding department. Return EmpID, EmpName,Salary in your output
-SQL Setup:
+----| Device_id | no_of_location | max_signal_location | no_of_signals |
+----|-----------|----------------|---------------------|---------------|
+----| 12        | 2              | Bangalore           | 6             |
+----| 13        | 2              | Secunderabad        | 5             |
+----7. Write a SQL to find all Employees who earn more than the average salary in their corresponding department. Return EmpID, EmpName,Salary in your output
+----SQL Setup:
 
 CREATE TABLE Employee (
   EmpID INT,
@@ -295,36 +294,36 @@ WHERE Salary > (
 );
 
 
-Expected Output:
+----Expected Output:
 
-| EmpID | EmpName | Salary |
-|-------|---------|--------|
-| 1001  | Mark    | 60000  |
-| 1004  | Peter   | 35000  |
-| 1005  | John    | 55000  |
-| 1007  | Donald  | 35000  |
-8. You are part of an office lottery pool where you keep a table of the winning lottery numbers along with a table of each ticket’s chosen numbers. If a ticket has some but not all the winning numbers, you win $10. If a ticket has all the winning numbers, you win $100. Calculate the total winnings for today’s drawing.
-Winning Numbers:
+----| EmpID | EmpName | Salary |
+----|-------|---------|--------|
+----| 1001  | Mark    | 60000  |
+----| 1004  | Peter   | 35000  |
+----| 1005  | John    | 55000  |
+----| 1007  | Donald  | 35000  |
+----8. You are part of an office lottery pool where you keep a table of the winning lottery numbers along with a table of each ticket’s chosen numbers. If a ticket has some but not all the winning numbers, you win $10. If a ticket has all the winning numbers, you win $100. Calculate the total winnings for today’s drawing.
+----Winning Numbers:
 
-|Number|
---------
-|  25  |
-|  45  |
-|  78  |
+----|Number|
+------------
+----|  25  |
+----|  45  |
+----|  78  |
 
-Tickets:
+----Tickets:
 
-| Ticket ID | Number |
-|-----------|--------|
-| A23423    | 25     |
-| A23423    | 45     |
-| A23423    | 78     |
-| B35643    | 25     |
-| B35643    | 45     |
-| B35643    | 98     |
-| C98787    | 67     |
-| C98787    | 86     |
-| C98787    | 91     |
+----| Ticket ID | Number |
+----|-----------|--------|
+----| A23423    | 25     |
+----| A23423    | 45     |
+----| A23423    | 78     |
+----| B35643    | 25     |
+----| B35643    | 45     |
+----| B35643    | 98     |
+----| C98787    | 67     |
+----| C98787    | 86     |
+----| C98787    | 91     |
 
 
 WITH WinningNumbers AS (
@@ -347,11 +346,11 @@ SELECT Total_Winnings FROM Results;
 
 
 
-Expected Output would be $110, as you have one winning ticket, and one ticket that has some but not all the winning numbers.
+----Expected Output would be $110, as you have one winning ticket, and one ticket that has some but not all the winning numbers.
 
-9. The Spending table keeps the logs of the spendings history of users that make purchases from an online shopping website which has a desktop and a mobile devices.
-Write an SQL query to find the total number of users and the total amount spent using mobile only, desktop only and both mobile and desktop together for each date.
-SQL Setup:
+----9. The Spending table keeps the logs of the spendings history of users that make purchases from an online shopping website which has a desktop and a mobile devices.
+----Write an SQL query to find the total number of users and the total amount spent using mobile only, desktop only and both mobile and desktop together for each date.
+----SQL Setup:
 
 CREATE TABLE Spending (
   User_id INT,
@@ -403,38 +402,38 @@ FROM Classified;
 
 
 
-Expected Output:
+----Expected Output:
 
-| Row | Spend_date | Platform | Total_Amount | Total_users |
-|-----|------------|----------|--------------|-------------|
-| 1   | 2019-07-01 | Mobile   | 100          | 1           |
-| 2   | 2019-07-01 | Desktop  | 100          | 1           |
-| 3   | 2019-07-01 | Both     | 200          | 1           |
-| 4   | 2019-07-02 | Mobile   | 100          | 1           |
-| 5   | 2019-07-02 | Desktop  | 100          | 1           |
-| 6   | 2019-07-02 | Both     | 0            | 0           |
-10. Write an SQL Statement to de-group the following data.
-Input Table: 'Grouped'
+----| Row | Spend_date | Platform | Total_Amount | Total_users |
+----|-----|------------|----------|--------------|-------------|
+----| 1   | 2019-07-01 | Mobile   | 100          | 1           |
+----| 2   | 2019-07-01 | Desktop  | 100          | 1           |
+----| 3   | 2019-07-01 | Both     | 200          | 1           |
+----| 4   | 2019-07-02 | Mobile   | 100          | 1           |
+----| 5   | 2019-07-02 | Desktop  | 100          | 1           |
+----| 6   | 2019-07-02 | Both     | 0            | 0           |
+----10. Write an SQL Statement to de-group the following data.
+----Input Table: 'Grouped'
 
-|Product  |Quantity|
---------------------
-|Pencil   |   3    |
-|Eraser   |   4    |
-|Notebook |   2    |
-Expected Output:
+----|Product  |Quantity|
+------------------------
+----|Pencil   |   3    |
+----|Eraser   |   4    |
+----|Notebook |   2    |
+----Expected Output:
 
-|Product  |Quantity|
---------------------
-|Pencil   |   1    |
-|Pencil   |   1    |
-|Pencil   |   1    |
-|Eraser   |   1    |
-|Eraser   |   1    |
-|Eraser   |   1    |
-|Eraser   |   1    |
-|Notebook |   1    |
-|Notebook |   1    |
-SQL Setup:
+----|Product  |Quantity|
+------------------------
+----|Pencil   |   1    |
+----|Pencil   |   1    |
+----|Pencil   |   1    |
+----|Eraser   |   1    |
+----|Eraser   |   1    |
+----|Eraser   |   1    |
+----|Eraser   |   1    |
+----|Notebook |   1    |
+----|Notebook |   1    |
+----SQL Setup:
 
 DROP TABLE IF EXISTS Grouped;
 CREATE TABLE Grouped
